93461931133fedaa9d043afa35a2e8c6f2b6dbe4
John Torres <jtorres0802@gmail.com>
2019-07-18 15:49:48 -0400

    Added elementary functions 4 & 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+// #headBadge = heading badge
+// #btn1 = button ID
+
 // First Trial Function.  Displays window with x + y result and writes to console.
 
 function1();
@@ -6,10 +9,9 @@ function function1() {
     var var1 = 5;
     var var2 = 10;
     var var3 = var1 + var2;
-    window.alert("Trial 1: var3 = " + var3);
+    //window.alert("Trial 1: var3 = " + var3);
     console.log("Trial 1: var3 = " + var3);
 }
-
 
 // Second Trial Function.  Changes "Welcome!" badge to "Hello!" badge and writes to console.
 
@@ -18,10 +20,9 @@ function2();
 function function2() {
 
     document.getElementById('headBadge').innerHTML = "Hello!";
-    window.alert("Trial 2: headBadge changed to " + headBadge.innerHTML);
+    //window.alert("Trial 2: headBadge changed to " + headBadge.innerHTML);
     console.log("Trial 2: headBadge changed to " + headBadge.innerHTML);
 }
-
 
 // Third Trial Function.  Alert window populates with "This is a test."
 
@@ -29,15 +30,36 @@ function3();
 
 function function3() {
     var trial3 = "This is a ";
-    window.alert("Trial 3: " + trial3 + "test.");
+    //window.alert("Trial 3: " + trial3 + "test.");
     console.log("Trial 3: " + trial3 + "test.");
 }
 
-// Fourth Trial.
+// Fourth Trial. Trying out typeof operator.
 
 function4();
 
 function function4() {
+    var trialline = "This is a ";
+    var trial4a = "test";
+    var trial4b = 5;
 
+    //window.alert("Trial 4a: " + trialline + "" + typeof trial4a);
+    //window.alert("Trial 4b: " + trialline + "" + typeof trial4b);
+    console.log("Trial 4a: " + trialline + "" + typeof trial4a);
+    console.log("Trial 4b: " + trialline + "" + typeof trial4b);
+    console.log("Trial 4c: " + trialline + "" + typeof function4);
 
 }
+
+// Fifth Trial. Event Listeners with Button Clicks.
+
+document.getElementById("btn1").addEventListener("click", function5);
+
+function function5() {
+    document.getElementById("btn1").innerHTML = "High Voltage!";
+    document.getElementById("headBadge").innerHTML = "Danger! Danger!";
+    console.log("Trial 5a: Danger button changes to High Voltage! button on click")
+    console.log("Trial 5b: Hello! headBadge changes to Danger Danger! headBadge on click")
+}
+
+// Sixth Trial.
